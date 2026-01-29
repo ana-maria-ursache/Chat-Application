d0c964ca7f7fe5123c11b7b286250c1014a1e15e
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-01-29 12:23:58 +0200

    added active users and their conv

--- server-ws.js
+++ server-ws.js
@@ -4,8 +4,16 @@ const io = new Server(4000, {
     cors: { origin: "http://localhost:3000" } // dam voie sa fie conexiuni de la UI la server
 });
 
+const onlineUsers = {};
+
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+
+    socket.on("user_join", (username) => {
+        onlineUsers[socket.id] = username;
+        console.log(username + " joined, online users:", onlineUsers);
+        io.emit("online_users_update", onlineUsers);
+    });
 
     socket.on("send_message", (data) => {
         io.emit("receive_message", data);
@@ -13,6 +21,8 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         console.log("This user disconnected:", socket.id);
+        delete onlineUsers[socket.id];
+        io.emit("online_users_update", onlineUsers);
     });
 });
 
