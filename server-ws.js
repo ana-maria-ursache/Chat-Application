a41931c0272f1d3feaf280c69931b9ca7ca2c633
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-01-29 09:25:45 +0200

    added 2 servers, 1 for the UI and 1 for the websocket

---
+++ server-ws.js
@@ -0,0 +1,19 @@
+const { Server } = require("socket.io");
+
+const io = new Server(4000, {
+    cors: { origin: "http://localhost:3000" } // dam voie sa fie conexiuni de la UI la server
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("send_message", (data) => {
+        io.emit("receive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("This user disconnected:", socket.id);
+    });
+});
+
+console.log("This websocket server is running on port 4000");
